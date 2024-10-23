b01d82dbbe712a8a9a6d861fbec03331e2047a4d
richard.kudela <richard.kudela@yunextraffic.com>
2024-10-23 15:58:42 +0200

    Red phase of US1

--- test/test_translator.py
+++ test/test_translator.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
+from src.translator import PigLatinTranslator
+
 
 class TestPigLatinTranslator(TestCase):
 
-    def test_something(self):
-        # Test method example
-        pass
+    def test_get_phrase(self):
+        word = "Hello word"
+        translator = PigLatinTranslator(word)
+        phrase = translator.get_phrase()
+        self.assertEqual(word, phrase)
